9e49e37790dff2e40bb9dfe21383510b9d611f34
Samuel Jones <samjones714@gmail.com>
2019-10-03 00:50:54 +0100

    Flesh out AppCamera Class and finish off basic design.

--- app/src/main/java/uk/aber/dcs/ac/uk/cs13620/CameraApp/AppCamera.java
+++ app/src/main/java/uk/aber/dcs/ac/uk/cs13620/CameraApp/AppCamera.java
@@ -1,36 +1,171 @@
 package uk.aber.dcs.ac.uk.cs13620.CameraApp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
-import android.media.Image;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
+import android.os.Build;
+import android.view.Surface;
+import android.view.TextureView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import java.util.Arrays;
 
 import static android.content.Context.CAMERA_SERVICE;
 
 public class AppCamera {
     // For use later when adding ability to select which camera to get image from.
-    private String[] cameraList;
+    private String[] m_cameraList;
+    private TextureView m_previewView;
+    private CameraCaptureSession m_previewCaptureSession;
+    private CameraDevice m_currentCameraDevice;
+    private CameraManager m_cameraManager;
+    private CaptureRequest m_previewRequest;
+    private Context m_context;
 
-    public AppCamera(Context context) {
-        CameraManager manager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
+    private static final int CAMERA_PREVIEW = 0;
+    private int m_cameraState;
+
+    private static final int PERMISSION_REQUEST_USE_CAMERA = 1;
+
+    public AppCamera(Context context, TextureView textureView) {
+        m_previewView = textureView;
+        m_cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
         try {
-            if (manager != null) {
-                cameraList = manager.getCameraIdList();
+            if (m_cameraManager != null) {
+                m_cameraList = m_cameraManager.getCameraIdList();
             } else {
                 System.out.println("[Debug] Manager is none existent at AppCamera Creation");
             }
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
-
-
+        m_cameraState = CAMERA_PREVIEW;
+        m_context = context;
     }
 
-    public Image captureImage() {
-        return new Image();
+
+    private final CameraDevice.StateCallback m_cameraDeviceStateCallBack = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            m_currentCameraDevice = camera;
+            // startCameraPreviewSession();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            camera.close();
+            m_currentCameraDevice = null;
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            camera.close();
+            m_currentCameraDevice = null;
+        }
+    };
+
+    private CameraCaptureSession.CaptureCallback m_captureCallback = new CameraCaptureSession.CaptureCallback() {
+        private void processCaptureCallback() {
+            // At present nothing should happen regardless of current state. Switch statement for future expansion.
+            switch (m_cameraState) {
+                case (CAMERA_PREVIEW): {
+                    //TODO: This is where processing of facial recognition would happen, or at least a callback would be called that is an optional plugin for preview.
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public void onCaptureProgressed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureResult partialResult) {
+            super.onCaptureProgressed(session, request, partialResult);
+            processCaptureCallback();
+        }
+
+        @Override
+        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+            processCaptureCallback();
+        }
+    };
+
+    public void startCameraPreviewSession(String cameraID, Activity activity) {
+        //TODO: Ensure that camera permissions are gained.
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (m_context.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_USE_CAMERA);
+                }
+                // Check again and confirm else return and give up.
+                if (m_context.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    System.out.println("[DEBUG] Permission for Camera not granted");
+                    return;
+                }
+            }
+            // Open the camera and have it assigned to m_currentCameraDevice using the CallBack
+            m_cameraManager.openCamera(cameraID, m_cameraDeviceStateCallBack, null);
+
+            SurfaceTexture texture = m_previewView.getSurfaceTexture();
+
+            Surface surface = new Surface(texture);
+
+            if (m_currentCameraDevice == null) {
+                System.out.println("[Debug] Current camera device is null, when starting preview session.");
+                return;
+            }
+
+            final CaptureRequest.Builder previewBuilder = m_currentCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            previewBuilder.addTarget(surface);
+
+
+            CameraCaptureSession.StateCallback captureSession = new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    // The camera doesn't exist/isn't open
+                    if (m_currentCameraDevice == null) {
+                        return;
+                    }
+
+                    m_previewCaptureSession = session;
+                    try {
+                        // Set autofocus to be on
+                        previewBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                        // TODO: Consider adding flash to be on when required i.e. AutoFlash.
+
+                        m_previewRequest = previewBuilder.build();
+                        m_previewCaptureSession.setRepeatingRequest(m_previewRequest, m_captureCallback, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                    System.out.println("[Debug] capture session callback onConfigureFailed called.");
+                }
+            };
+
+            m_currentCameraDevice.createCaptureSession(Arrays.asList(surface), captureSession, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
+
+//    public Image captureImage() {
+//        return new Image();
+//    }
 
     public String[] getCameraList() {
-        return cameraList;
+        return m_cameraList;
     }
 }
--- app/src/main/java/uk/aber/dcs/ac/uk/cs13620/CameraApp/MainActivity.java
+++ app/src/main/java/uk/aber/dcs/ac/uk/cs13620/CameraApp/MainActivity.java
@@ -1,27 +1,93 @@
 package uk.aber.dcs.ac.uk.cs13620.CameraApp;
 
-import android.media.Image;
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppCamera camera;
+    private AppCamera m_camera;
+    private TextureView m_previewView;
+    private String m_currentCameraID;
+    private Button m_camButton;
+    private Spinner m_camIdSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        m_previewView = findViewById(R.id.textureView);
+        m_camButton = findViewById(R.id.camButton);
+        m_camIdSpinner = findViewById(R.id.camIdSpinner);
+
+        m_camera = new AppCamera(this.getApplicationContext(), m_previewView);
+
+        populateCamIdSpinner();
         createCamPreview();
     }
 
     public void camButtonClicked(View view) {
-        Image image = camera.captureImage();
+        //Image image = camera.captureImage();
+        System.out.println("[Debug] Cam button clicked!");
+    }
+
+    private class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            m_currentCameraID = parent.getItemAtPosition(position).toString();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            // This shouldn't occur so log
+            System.out.println("[Debug] Nothing selected in spinner!");
+        }
+    }
+
+    private void populateCamIdSpinner() {
+        String[] camIDs = m_camera.getCameraList();
+        if (camIDs.length < 1) {
+            System.out.println("[Debug] No CamIDs found");
+            return;
+        }
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, R.layout.support_simple_spinner_dropdown_item, camIDs);
+        m_camIdSpinner.setAdapter(adapter);
+        m_camIdSpinner.setOnItemSelectedListener(new SpinnerActivity());
+        m_currentCameraID = camIDs[0];
+    }
+
+    private class SurfaceTextureListener extends Activity implements TextureView.SurfaceTextureListener {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            m_camera.startCameraPreviewSession(m_currentCameraID, this);
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+            // Do nothing
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            // Do nothing
+        }
     }
 
     private void createCamPreview() {
+        m_previewView.setSurfaceTextureListener(new SurfaceTextureListener());
     }
 }
